3f1a1c7a5715edba4f4b2b7d533834822d8f6f79
mirmic80 <mirco.michel@bluewin.ch>
2021-12-05 17:10:04 +0100

    Inserted own method after Line 82, generate random number. Does not work yet

--- SlotMachineNew/Program.cs
+++ SlotMachineNew/Program.cs
@@ -19,60 +19,82 @@ slotMachineLine[0] = new int[3];
 slotMachineLine[1] = new int[3];
 slotMachineLine[2] = new int[3];
 
-while (wantsToPlayGame)
+//while (wantsToPlayGame)
+//{
+//    Console.WriteLine("Please your wager");
+//    input = Console.ReadLine();
+//    wager = int.Parse(input);
+
+//    //for (int z = 0; z < spinNumber; z++)
+//    //{
+//    Console.Clear();
+//    // Display the array elements.
+//    for (int i = 0; i < slotMachineLine.Length; i++)
+//    {
+//        //Fills the random number into the array
+//        for (int j = 0; j < slotMachineLine[i].Length; j++)
+//        {
+//            Random RandomLucky = new Random();
+//            int luckyNumber = RandomLucky.Next(luckNumberMin, luckyNumberMax);
+//            slotMachineLine[i][j] = luckyNumber;
+//            System.Console.Write(slotMachineLine[i][j]);
+//        }
+//        Console.WriteLine();
+//    }
+//    //}
+
+//    Console.WriteLine(NumberSlotmachine);
+
+//    //Compares the numbers on the middle row 
+//    if (slotMachineLine[1][0] == slotMachineLine[1][1] && slotMachineLine[1][0] == slotMachineLine[1][2])
+//    {
+//        Console.WriteLine("Jackpot you are the winner");
+//    }
+//    else
+//    {
+//        Console.WriteLine("you lost");
+//    }
+
+
+//    Console.WriteLine("press E to exit the game or press another tap for a new game");
+
+//    bool correctKey = false;
+//    while (!correctKey)
+//    {
+
+//        ConsoleKeyInfo info = Console.ReadKey(true);
+
+//        if (info.Key != ConsoleKey.E)
+//        {
+//            wantsToPlayGame = true;
+//            correctKey = true;
+//        }
+
+//        if (info.Key == ConsoleKey.E)
+//        {
+//            wantsToPlayGame = false;
+//            correctKey = true;
+//        }
+//    }
+    
+//}
+
+int testLuckyNumber = NumberSlotmachine(slotMachineLine.Length, slotMachineLine.Count);
+
+Console.WriteLine(testLuckyNumber);
+
+static int NumberSlotmachine(int dimension, int index, int lucky)
 {
-    Console.WriteLine("Please your wager");
-    input = Console.ReadLine();
-    wager = int.Parse(input);
-
-    //for (int z = 0; z < spinNumber; z++)
-    //{
-        Console.Clear();
-        // Display the array elements.
-        for (int i = 0; i < slotMachineLine.Length; i++)
+    int NumberSlotMachine = lucky;
+    for (int i = 0; i < dimension; i++)
+    { 
+        for (int j = 0; j < index; j++)
         {
-            //Fills the random number into the array
-            for (int j = 0; j < slotMachineLine[i].Length; j++)
-            {
-                Random RandomLucky = new Random();
-                int luckyNumber = RandomLucky.Next(luckNumberMin, luckyNumberMax);
-                slotMachineLine[i][j] = luckyNumber;
-                System.Console.Write(slotMachineLine[i][j]);
-            }
-            Console.WriteLine();
+            Random number = new Random();
+            int lucky = number.Next(0, 3);
+
         }
-    //}
-
-    //Compares the numbers on the middle row 
-    if (slotMachineLine[1][0] == slotMachineLine[1][1] && slotMachineLine[1][0] == slotMachineLine[1][2])
-    {
-        Console.WriteLine("Jackpot you are the winner");
-    }
-    else
-    {
-        Console.WriteLine("you lost");
-    }
-
-    Console.WriteLine("press E to exit the game or press another tap for a new game");
-
-    bool correctKey = false;
-    while (!correctKey)
-    {
-
-        ConsoleKeyInfo info = Console.ReadKey(true);
-
-        if (info.Key != ConsoleKey.E)
-        {
-            wantsToPlayGame = true;
-            correctKey = true;
-        }
-
-        if (info.Key == ConsoleKey.E)
-        {
-            wantsToPlayGame = false;
-            correctKey = true;
-        }
-    }
+    }   
+    return NumberSlotMachine;
 }
-
 
